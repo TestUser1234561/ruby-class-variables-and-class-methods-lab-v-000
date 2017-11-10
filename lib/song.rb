34ad8a895cd412bddf13127846d54c6b08e0553e
Morgan Fudge <mfudge92@gmail.com>
2017-11-10 08:27:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,17 @@
 class Song
 
   attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artist = Array.new
+  @@genre = Array.new
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@artist << artist
+    @@genre << genre
   end
 
 end
